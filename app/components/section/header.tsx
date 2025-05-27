4fd17f74d1165dfab0a887ca19371256b5e1962c
AplusADV <hello.aplusadvertising@gmail.com>
2025-05-27 10:28:30 +0700

    mobile responsive#3

--- app/components/section/header.tsx
+++ app/components/section/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className='w-full h-full flex flex-col md:flex-row items-center justify-center gap-2 py-8 md:py-16 px-4 md:px-24 lg:px-32 relative'>
       {/* <div className='absolute bottom-0 left-0 w-full h-full -z-10 bg-gradient-to-tr from-[#000000] to-transparent ' /> */}
       <div className=' flex flex-col  gap-4 w-full md:w-[50%] '>
-        <h1 className='text-white text-xl sm:text-3xl md:text-5xl lg:text-7xl leading-tight'>Transforming Ideas Into Impactful <span className='font-extrabold text-[#f7a619]'>Advertising</span> Solutions</h1>
+        <h1 className='text-white text-3xl md:text-5xl lg:text-7xl leading-tight'>Transforming Ideas Into Impactful <span className='font-extrabold text-[#f7a619]'>Advertising</span> Solutions</h1>
         <p className='text-white/70 text-xl '>At APLUS ADVERTISING, we specialize in crafting innovative marketting strategies that resonate with your audience. </p>
         <Button variant='outline' size="lg" className='bg-[#f7a619] text-black hover:bg-[#f7a619]/90 mt-4 w-fit outline-none border-none text-lg'>
           <Link href="https://wa.me/6281212345678" target='_blank'>About Us</Link>
